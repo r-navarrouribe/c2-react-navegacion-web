b2f80efaf29583ee4042c8f5f55265d43b9c8bea
Raúl Navarro <raul.navarro.uribe@gmail.com>
2021-06-10 23:22:00 +0200

    fin

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [numero, setNumero] = useState(1);
+  const botones = [
+    { id: 1, numero: numero },
+    { id: 2, numero: numero + 1 },
+    { id: 3, numero: numero + 2 },
+    { id: 4, numero: numero + 3 },
+    { id: 5, numero: numero + 4 },
+  ];
+  const [numeroClickado, setNumeroClickado] = useState("");
+  const [mensaje, setMensaje] = useState("");
+  const sumar = () => {
+    if (numero === 16) {
+      const nuevoMensaje = "Ya no hay más";
+      setMensaje(nuevoMensaje);
+      return;
+    }
+    const nuevoMensaje = "";
+    setMensaje(nuevoMensaje);
+    setNumero(numero + 1);
+  };
+  const restar = () => {
+    if (numero === 0) {
+      const nuevoMensaje = "Ya no hay menos";
+      setMensaje(nuevoMensaje);
+      return;
+    }
+    const nuevoMensaje = "";
+    setMensaje(nuevoMensaje);
+    setNumero(numero - 1);
+  };
+  const enviaNumero = (numero) => {
+    const nuevoNumeroClickado = numero;
+    setNumeroClickado(nuevoNumeroClickado);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="contenedor">
+        <div className="contenedor-botones">
+          <button className="boton boton-cambiar" onClick={restar}>
+            «
+          </button>
+          {botones.map((boton) => (
+            <button
+              className="boton boton-numero"
+              key={boton.numero}
+              onClick={() => {
+                enviaNumero(boton.numero);
+              }}
+            >
+              {boton.numero}
+            </button>
+          ))}
+          <button className="boton boton-cambiar" onClick={sumar}>
+            »
+          </button>
+        </div>
+        <p className="mensaje">{mensaje}</p>
+        <p className="numero-clickado">{numeroClickado}</p>
+      </div>
+    </>
   );
 }
 
